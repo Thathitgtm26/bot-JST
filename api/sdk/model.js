68ff6f3c63da839b86668b472cdfba824dae7070
Thathitgtm26 <95642687+Thathitgtm26@users.noreply.github.com>
2021-12-31 22:40:18 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Thathitgtm26/bot-jst/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Thathitgtm26/bot-jst/main/public/ex_model/model.json?token=5001348467:AAHIeGBZ9etH_j3eDhEADJ4ipFAfQYEytX0';
         const model1_7 = await tf.loadGraphModel(path);
         
         predict = model1_7.predict(
