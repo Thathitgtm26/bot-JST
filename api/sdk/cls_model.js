a7feddb193e495678d6c17eb6c95ea92276c8740
Thathitgtm26 <95642687+Thathitgtm26@users.noreply.github.com>
2021-12-31 20:21:56 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -1,11 +1,13 @@
 const tf = require('@tensorflow/tfjs-node');
 
-function normalized(data){ // i & r
-    i = (data[0] - 12.585) / 6.813882
-    r = (data[1] - 51.4795) / 29.151289
-    v = (data[2] - 650.4795) / 552.6351
-    p = (data[3] - 10620.56) / 12152.78
-    return [i, r, v, p]
+function normalized(data){ // x1, x2, & x3
+    x1 = (data[0] - 42.6633) / 10.6153
+    x2 = (data[1] - 88.7367) / 19.014
+    x3 = (data[2] - 143.048) / 23.0612
+    y1 = (data[3] - 74.7378) / 9,18444
+    y2 = (data[4] - 49.8289) / 14.7223
+    y3 = (data[5] - 159.709) / 23.9795
+    return [x1, x2, x3, y1, y2, y3]
 }
 
 const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]).reduce(compareFn)[1]
@@ -14,8 +16,8 @@ const argMax = argFact((min, el) => (el[0] > min[0] ? el : min))
 function ArgMax(res){
     label = "NORMAL"
     cls_data = []
-    for(i=0; i<res.length; i++){
-        cls_data[i] = res[i]
+    for(x1=0; x1<res.length; x1++){
+        cls_data[x1] = res[x1]
     }
     // 
     
@@ -28,7 +30,7 @@ function ArgMax(res){
 }
 
 async function classify(data){
-    let in_dim = 4; // i r v p
+    let in_dim = 6; // x1 x2 x3 y1 y2 y3
     
     data = normalized(data);
     shape = [1, in_dim];
